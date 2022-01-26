bbb6ce7917e2a8ef1f3dc6990fcacd4f3b075acd
Yilei Yang <yileiyang@google.com>
2022-01-25 22:05:48 -0800

    Use math.gcd instead of fractions.gcd, the former is deprecated in Python 3.5 and removed in 3.9.
    PiperOrigin-RevId: 424259166

--- mesh_tensorflow/auto_mtf/valid_layouts.py
+++ mesh_tensorflow/auto_mtf/valid_layouts.py
@@ -27,7 +27,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import fractions
+import math
 import re
 
 
@@ -132,7 +132,7 @@ class LayoutValidator(object):
     for mtf_operation in mtf_graph.operations:
       for mtf_tensor in mtf_operation.outputs:
         for mtf_dimension in mtf_tensor.shape.dims:
-          mtf_dimension_name_to_size_gcd[mtf_dimension.name] = fractions.gcd(
+          mtf_dimension_name_to_size_gcd[mtf_dimension.name] = math.gcd(
               mtf_dimension_name_to_size_gcd.get(mtf_dimension.name,
                                                  mtf_dimension.size),
               mtf_dimension.size)
